66a4af847385c3d8dbe83607145abd1e17fdec19
LittleBlue418 <hollyathomas88@gmail.com>
2020-03-19 13:00:31 +0100

    Get port from environment

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def careers():
 
 if __name__ == "__main__":
     # remove debug=true before assessment!!
-    app.run(port=5000, debug=True)
+    app.run(port=int(os.environ.get('PORT', 5000)), debug=True)
