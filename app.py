a8165ae6e0dd0b146a268ed1c285c1d6d990c0ba
LittleBlue418 <hollyathomas88@gmail.com>
2020-03-19 13:08:18 +0100

    Try to bind to 0.0.0.0

--- app.py
+++ app.py
@@ -40,4 +40,8 @@ def careers():
 
 if __name__ == "__main__":
     # remove debug=true before assessment!!
-    app.run(port=int(os.environ.get('PORT', 5000)), debug=True)
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000)),
+        debug=True
+    )
